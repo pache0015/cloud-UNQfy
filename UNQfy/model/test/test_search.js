f365ca87c8af4a2808f608c0052a552b08972cf1
underabloodysky <valenzuelahoracioe@gmail.com>
2020-09-13 23:13:39 -0300

    Agrego tests de searcher por nombre parcial

--- UNQfy/model/test/test_search.js
+++ UNQfy/model/test/test_search.js
@@ -5,7 +5,7 @@ const Searcher = require('../src/Searcher.js');
 
 class DummyIdentificable{
   constructor(aName){
-    this.name = aName
+    this.name = aName;
   }
 }
 
@@ -19,144 +19,60 @@ describe('Search entities with a partial name', () => {
   });
 
   it('should return a empty list when search in a empty list', () => {
-    let searchInAEmptyList = my_search.searchAllWithPartialName([],"Sarasa",)
+    let searchInAEmptyList = my_search.searchAllWithPartialName([],"Sarasa",);
     assert.lengthOf(searchInAEmptyList, 0);
   });
 
   it('should return a list with an unique object when the list has a uniq element and its name matchs exactly  with the argument', () => {
-    let searchInAEmptyList = my_search.searchAllWithPartialName(list_with_a_uniq_identificable,"Sarasa",)
-    assert.lengthOf(searchInAEmptyList, 1);
-    assert.isTrue(searchInAEmptyList.includes(uniq_identificable))
+    let resultList = my_search.searchAllWithPartialName(list_with_a_uniq_identificable,"Sarasa",);
+    assert.lengthOf(resultList, 1);
+    assert.isTrue(resultList.includes(uniq_identificable));
   });
 
   it('should return a list with an unique object when the list has a uniq element and its name matchs exactly, but in UpperCase  ', () => {
-    let searchInAEmptyList = my_search.searchAllWithPartialName(list_with_a_uniq_identificable,"SARASA",)
-    assert.lengthOf(searchInAEmptyList, 1);
-    assert.isTrue(searchInAEmptyList.includes(uniq_identificable))
+    let resultList = my_search.searchAllWithPartialName(list_with_a_uniq_identificable,"SARASA",);
+    assert.lengthOf(resultList, 1);
+    assert.isTrue(resultList.includes(uniq_identificable));
   });
 
   it('should return a list with an unique object when the list of the argument has a unique object when the name matchs exactly but in LowerCase  ', () => {
-    let searchInAEmptyList = my_search.searchAllWithPartialName(list_with_a_uniq_identificable,"sarasa",)
-    assert.lengthOf(searchInAEmptyList, 1);
-    assert.isTrue(searchInAEmptyList.includes(uniq_identificable))
+    let resultList = my_search.searchAllWithPartialName(list_with_a_uniq_identificable,"sarasa",);
+    assert.lengthOf(resultList, 1);
+    assert.isTrue(resultList.includes(uniq_identificable));
   });
 
-//  it('', () => {
-//    let list_with_uniq_identificable = [new ("Sarasa y sus amigos")]
-//    let searchInAEmptyList = my_search.searchAllWithPartialName([],"Sarasa",)
-//    assert.lengthOf(searchInAEmptyList, 0);
-//  });
-//
-//
-//  it('', () => {
-//    let list_with_uniq_identificable = [new ("Sarasa y sus amigos")]
-//    let searchInAEmptyList = my_search.searchAllWithPartialName([],"Sarasa",)
-//    assert.lengthOf(searchInAEmptyList, 0);
-//  });
-//
-// it('', () => {
-//    let list_with_uniq_identificable = [new ("Sarasa y sus amigos")]
-//    let searchInAEmptyList = my_search.searchAllWithPartialName([],"Sarasa",)
-//    assert.lengthOf(searchInAEmptyList, 0);
-//  });
-//});
+  it('should return a list with an unique object when the list of the argument has a unique object when argument is its partial name  ', () => {
+    let resultList = my_search.searchAllWithPartialName(list_with_a_uniq_identificable,"sarasa",);
+    assert.lengthOf(resultList, 1);
+    assert.isTrue(resultList.includes(uniq_identificable));
+  });
 
-//    assert.equal(track.name, 'Welcome to the jungle');
-//    assert.strictEqual(track.duration, 200);
-//    assert.equal(track.genres.includes('rock'), true);
-//    assert.equal(track.genres.includes('hard rock'), true);
-//    assert.lengthOf(track.genres, 2);
-//  });
+  it('should return a list with an unique object when the list of the argument has a unique object when argument is its partial name between other Identificables with different name', () => {
+    let resultList = my_search.searchAllWithPartialName(list_witH_a_uniq_identificable_with_others,"Saras",)
+    assert.lengthOf(resultList, 1);
+    assert.isTrue(resultList.includes(uniq_identificable));
+  });
 
-//  it('should find different things by name', () => {
-//    const artist1 = createAndAddArtist(unqfy, 'Guns n\' Roses', 'USA');
-//    const album1 = createAndAddAlbum(unqfy, artist1.id, 'Roses Album', 1987);
-//    const track = createAndAddTrack(unqfy, album1.id, 'Roses track', 200, ['pop', 'movie']);
-//    const playlist = unqfy.createPlaylist('Roses playlist', ['pop'], 1400);
-//
-//    const results = unqfy.searchByName('Roses');
-//    assert.deepEqual(results, {
-//      artists: [artist1],
-//      albums: [album1],
-//      tracks: [track],
-//      playlists: [playlist],
-//    });
-//  });
-//
-//  it('should get all tracks matching genres', () => {
-//    const artist1 = createAndAddArtist(unqfy, 'Guns n\' Roses', 'USA');
-//    const album1 = createAndAddAlbum(unqfy, artist1.id, 'Appetite for Destruction', 1987);
-//    const t0 = createAndAddTrack(unqfy, album1.id, 'Welcome to the jungle', 200, ['rock', 'hard rock', 'movie']);
-//    const t1 = createAndAddTrack(unqfy, album1.id, 'Sweet Child o\' Mine', 500, ['rock', 'hard rock', 'pop', 'movie']);
-//
-//    const artist2 = createAndAddArtist(unqfy, 'Michael Jackson', 'USA');
-//    const album2 = createAndAddAlbum(unqfy, artist2.id, 'Thriller', 1987);
-//    const t2 = createAndAddTrack(unqfy, album2.id, 'Trhiller', 200, ['pop', 'movie']);
-//    createAndAddTrack(unqfy, album2.id, 'Another song', 500, ['classic']);
-//    const t3 = createAndAddTrack(unqfy, album2.id, 'Another song II', 500, ['movie']);
-//
-//    const tracksMatching = unqfy.getTracksMatchingGenres(['pop', 'movie']);
-//
-//    // assert.equal(tracks.matching.constructor.name, Array);
-//    assert.isArray(tracksMatching);
-//    assert.lengthOf(tracksMatching, 4);
-//    assert.equal(tracksMatching.includes(t0), true);
-//    assert.equal(tracksMatching.includes(t1), true);
-//    assert.equal(tracksMatching.includes(t2), true);
-//    assert.equal(tracksMatching.includes(t3), true);
-//  });
-//
-//  it('should get all tracks matching artist', () => {
-//    const artist = createAndAddArtist(unqfy, 'Guns n\' Roses', 'USA');
-//    const album = createAndAddAlbum(unqfy, artist.id, 'Appetite for Destruction', 1987);
-//    const t1 = createAndAddTrack(unqfy, album.id, 'Welcome to the jungle', 200, ['rock', 'hard rock']);
-//    const t2 = createAndAddTrack(unqfy, album.id, 'It\'s so easy', 200, ['rock', 'hard rock']);
-//
-//    const album2 = createAndAddAlbum(unqfy, artist.id, 'Use Your Illusion I', 1992);
-//    const t3 = createAndAddTrack(unqfy, album2.id, 'Don\'t Cry', 500, ['rock', 'hard rock']);
-//
-//    const artist2 = createAndAddArtist(unqfy, 'Michael Jackson', 'USA');
-//    const album3 = createAndAddAlbum(unqfy, artist2.id, 'Thriller', 1987);
-//    createAndAddTrack(unqfy, album3.id, 'Thriller', 200, ['pop', 'movie']);
-//    createAndAddTrack(unqfy, album3.id, 'Another song', 500, ['classic']);
-//    createAndAddTrack(unqfy, album3.id, 'Another song II', 500, ['movie']);
-//
-//    const matchingTracks = unqfy.getTracksMatchingArtist(artist.name);
-//
-//    assert.isArray(matchingTracks);
-//    assert.lengthOf(matchingTracks, 3);
-//    assert.isTrue(matchingTracks.includes(t1));
-//    assert.isTrue(matchingTracks.includes(t2));
-//    assert.isTrue(matchingTracks.includes(t3));
-//  });
-//});
-//
-//describe('Playlist Creation and properties', () => {
-//  let unqfy = null;
-//
-//  beforeEach(() => {
-//    unqfy = new libunqfy.UNQfy();
-//  });
-//
-//  it('should create a playlist as requested', () => {
-//    const artist = createAndAddArtist(unqfy, 'Guns n\' Roses', 'USA');
-//    const album = createAndAddAlbum(unqfy, artist.id, 'Appetite for Destruction', 1987);
-//    const t1 = createAndAddTrack(unqfy, album.id, 'Welcome to the jungle', 200, ['rock', 'hard rock', 'movie']);
-//    createAndAddTrack(unqfy, album.id, 'Sweet Child o\' Mine', 1500, ['rock', 'hard rock', 'pop', 'movie']);
-//
-//    const artist2 = createAndAddArtist(unqfy, 'Michael Jackson', 'USA');
-//    const album2 = createAndAddAlbum(unqfy, artist2.id, 'Thriller', 1987);
-//    const t2 = createAndAddTrack(unqfy, album2.id, 'Thriller', 200, ['pop', 'movie']);
-//    const t3 = createAndAddTrack(unqfy, album2.id, 'Another song', 500, ['pop']);
-//    const t4 = createAndAddTrack(unqfy, album2.id, 'Another song II', 500, ['pop']);
-//
-//    const playlist = unqfy.createPlaylist('my playlist', ['pop', 'rock'], 1400);
-//
-//    assert.equal(playlist.name, 'my playlist');
-//    assert.isAtMost(playlist.duration(), 1400);
-//    assert.isTrue(playlist.hasTrack(t1));
-//    assert.isTrue(playlist.hasTrack(t2));
-//    assert.isTrue(playlist.hasTrack(t3));
-//    assert.isTrue(playlist.hasTrack(t4));
-//    assert.lengthOf(playlist.tracks, 4);
+  it('should return a list with an unique object when the list of the argument has a unique object when argument is its partial name in UppererCase, between other Identificables with different name', () => {
+    let resultList = my_search.searchAllWithPartialName(list_witH_a_uniq_identificable_with_others,"SARA",)
+    assert.lengthOf(resultList, 1);
+    assert.isTrue(resultList.includes(uniq_identificable));
+  });
+
+  it('should return a list with an unique object when the list of the argument has a unique object when argument is its partial name in LowerCase, between other Identificables with different name', () => {
+    let resultList = my_search.searchAllWithPartialName(list_witH_a_uniq_identificable_with_others,"saras",)
+    assert.lengthOf(resultList, 1);
+    assert.isTrue(resultList.includes(uniq_identificable));
+  });
+
+  it('should return a empty list when the object is not in the list of the argument', () => {
+    let resultList = my_search.searchAllWithPartialName(list_witH_diferent_identificables,"saras",)
+    assert.lengthOf(resultList, 0);
+  });
+
+  it('should return a list with 3 objects when the partial name matchs with the name of 3 identificables', () => {
+    let resultList = my_search.searchAllWithPartialName(list_witH_diferent_identificables,"Pepe",)
+    assert.lengthOf(resultList, 3);
+  });
+
 });
