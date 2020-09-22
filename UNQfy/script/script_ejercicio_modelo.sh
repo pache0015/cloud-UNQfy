5ff1d37f841cbb34f8fbbfb14eb734c450b6891c
underabloodysky <valenzuelahoracioe@gmail.com>
2020-09-22 09:20:31 -0300

    Script terminado

--- UNQfy/script/script_ejercicio_modelo.sh
+++ UNQfy/script/script_ejercicio_modelo.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-cd ..;
-
-
-
+cd ..
 
 # ARTISTS >>>>>>>>>>>>>>>>>>>>>>>>>>><
 
@@ -14,35 +11,33 @@ node main.js addArtist "Astor Piazzolla" "Argentina"
 
 # ALBUMS >>>>>>>>>>>>>>>>>>>>>>>>>>><
 
-node main.js addAlbum 1 "Despedazado por mil partes" 1995
-node main.js addAlbum 1"La Renga" 1 1998
-node main.js addAlbum 2"Raro" 6 2006
-node main.js addAlbum 2"Raro" 6 2006
-node main.js addAlbum 3"F.A.M.E." 4 2018
-node main.js addAlbum 3"A donde jugaran las niñas" 5 1997
-node main.js addAlbum 4"Raro" 6 2006
-node main.js addAlbum 4"Raro" 6 2006
+node main.js addAlbum 1 "Vida" 2010
+node main.js addAlbum 1 "Muerte" 2012
+node main.js addAlbum 2 "Blue" 2006
+node main.js addAlbum 3 "Greater hits" 1974
+node main.js addAlbum 3 "A donde jugaran las niñas" 5 1997
+node main.js addAlbum 4 "Piazzolla Project" 2006
 
 
 # TRACKS: >>>>>>>>>>>>>>>>>>>>>>>>>>><
 
 
-node main.js addTrack 1 "Desnudo para siempre" 248 Rock Heavy
-node main.js addTrack 1 "A la carga mi rock and roll" 336 Rock Heavy
-node main.js addTrack 1 "Cuando Vendran" 262 Rock Heavy
-node main.js addTrack 1 "Psilosibe Mexicana" 1 334 Rock Pop
-node main.js addTrack 2 "El final es en donde parti" 1 277 Rock
-node main.js addTrack 2 "El terco" 2 270 Rock Alternativo
-node main.js addTrack 2 "Tripa y corazon" 2 171 Rock Clasico
-node main.js addTrack 2 "Bien alto" 2 206 Rock
-node main.js addTrack 3 "El hombre de la estrella" 2 202 Rock
-node main.js addTrack 3 "El revelde" 2 224 Rock
-node main.js addTrack 3 "El aparato" 3 198 Pop Alternativo
-node main.js addTrack 3 "La ingrata" 3 212 Pop Jazz
-node main.js addTrack 4 "El ciclon" 3 255 Pop Alternativo
-node main.js addTrack 4 "El borrego" 3 188 Pop Alternativo
-node main.js addTrack 4 "Esa noche" 3 246 Pop Clasico
-node main.js addTrack 4 "Genesis" 4 266 Pop Alternativo
+node main.js addTrack 1 "Maquiavelico" 248 Rock Rap
+node main.js addTrack 1 "Es epico" 336 Rock Heavy Rap Clasico Rap
+node main.js addTrack 1 "Advertencia" 262 Rock Heavy Clasico Rap
+node main.js addTrack 1 "Paseo por el barrio" 334 Rock Pop Rap
+node main.js addTrack 2 "Ciudad de piedra" 277 Rock Rap
+node main.js addTrack 2 "Stop" 270 Rock Alternativo Rap
+node main.js addTrack 2 "Jeremias" 171 Rock Clasico Rap
+node main.js addTrack 2 "A name" 206 Rock
+node main.js addTrack 3 "El hombre de la estrella" 202 Rock
+node main.js addTrack 3 "El revelde" 224 Rock
+node main.js addTrack 3 "El aparato" 198 Pop Alternativo
+node main.js addTrack 3 "La ingrata" 212 Pop Jazz
+node main.js addTrack 4 "Black Dog" 255 Clasico Heavy Rock
+node main.js addTrack 4 "Since i have been loving you" 188 Pop Alternativo
+node main.js addTrack 4 "Dazed and confused" 246 Pop Clasico
+node main.js addTrack 6 "Adios nonino" 266 Blues Clasica
 
 
 # PLAYLISTS  >>>>>>>>>>>>>>>>>>>>>>>>>>><
@@ -56,9 +51,9 @@ node main.js createPlaylist "Relaxing Music" 1400 Blues Clasica
 node main.js addUser Pache0015
 node main.js addUser H
 
-node main.js userListenTrack 1 5
-node main.js userListenTrack 1 2
+node main.js userListenTrack 1 3
+node main.js userListenTrack 1 4
 node main.js userListenTrack 2 1
 node main.js userListenTrack 2 2
-node main.js userListenTrack 2 2
-node main.js userListenTrack 2 2
+node main.js userListenTrack 2 3
+node main.js userListenTrack 2 4
