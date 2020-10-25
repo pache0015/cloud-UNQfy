a19ab95b68e7e8661b195e76252a21fec19a4b9a
underabloodysky <valenzuelahoracioe@gmail.com>
2020-10-24 23:09:11 -0300

    primer approach de api

---
+++ UNQfy/model/api/api.js
@@ -0,0 +1,43 @@
+const bodyParser = require('body-parser');
+const express  = require('express'); 
+const app = express();                 
+const {
+    prueba
+    } = require('./routers.js');
+
+const port = 8080;  // set our port
+
+app.use((req, res, next) => {
+    bodyParser.json()(req, res, err => {
+        if (err) {
+            throw err;
+        }
+        next();
+    });
+});
+app.use('/api', prueba);
+const server = app.listen(port, () => {
+    console.log("Server running");
+});
+
+if (process.platform === "win32") {
+    var rl = require("readline").createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    rl.on("SIGINT", function () {
+        process.emit("SIGINT");
+    });
+}
+
+process.on("SIGINT", function () {
+    //graceful shutdown
+    console.log("Killing process and shutdown server");
+    server.close(() => {
+        console.log("Server OFF");
+        process.exit();
+    });
+});
+
+console.log("Listening in the port %d...", port);
---
+++ UNQfy/model/api/routers.js
@@ -0,0 +1,21 @@
+const express = require('express');        
+const artists = express.Router();
+const albums = express.Router();
+const prueba = express.Router();
+//const {getUNQfy,
+//       saveUNQfy} = require('../../main.js'); // para poder cargar/guarfar a UNQfy
+//
+//ENDPOINT /artists/<artistID>
+prueba.route('/prueba')
+.get((req, res) => {
+    res.status(200);
+    res.json(
+        "Hola"
+    );
+});
+
+module.exports = {
+  //  artists,
+  //  albums,
+  prueba
+};
