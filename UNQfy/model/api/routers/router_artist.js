e01a33d36dc39a47a1f1c96b631fe0a0802ca980
underabloodysky <valenzuelahoracioe@gmail.com>
2020-10-26 04:03:29 -0300

    Segundo endpoint de Artists funcionando

--- UNQfy/model/api/routers/router_artist.js
+++ UNQfy/model/api/routers/router_artist.js
@@ -3,18 +3,40 @@ const artists_router = express.Router();
 const {getUNQfy, saveUNQfy} = require('../../persistencia/persistenceManager.js');
 
 artists_router.route('/artists/:artist_id')
-.get((req, res) => {
-    const unqfy = getUNQfy();
-    const artist_ID = parseInt(req.params.artist_id);
-    const artist = unqfy.getArtistById(artist_ID);
-    console.log(unqfy.getArtists());
-    if (artist === undefined){
-        res.status(405);
-        res.json({status: 405, errorCode: "RELATED_RESOURCE_NOT_FOUND"});
-    }
-    else{
-        res.status(200);
-        res.json({body: artist.toJSON()});
-    }});
+    .get((req, res) => {
+        const unqfy = getUNQfy();
+        const artist_ID = parseInt(req.params.artist_id);
+       const artist = unqfy.getArtistById(artist_ID);
+        if (artist === undefined){
+            res.status(405);
+            res.json({status: 405, errorCode: "RELATED_RESOURCE_NOT_FOUND"});
+        }
+        else{
+            res.status(200);
+            res.json({body: artist.toJSON()});
+        }
+    });
+
+artists_router.route('/artists')
+    .get((req, res) => {
+        const unqfy = getUNQfy();
+        const artist_name = req.query.artist_name;
+        console.log(req.query);
+        console.log(artist_name);
+        if(artist_name === undefined){
+            res.status(400);
+            res.json({status: 400, errorCode: "BAD_REQUEST"});
+        }
+        else{
+            const artists = unqfy.searchArtistsWithPartialName(artist_name);
+            res.status(200);
+            res.json(
+                artists.map(artist => artist.toJSON())
+            );
+        }
+    });
+        
+        
+
 
 module.exports = artists_router;
--- UNQfy/model/persistencia/persistenceManager.js
+++ UNQfy/model/persistencia/persistenceManager.js
@@ -1,18 +1,16 @@
 const fs = require('fs'); // necesitado para guardar/cargar unqfy
-const unqmod = require('../../unqfy'); // importamos el modulo unqfy
+const unqmod = require('../../unqfy.js'); // importamos el modulo unqfy
 
 
-function getUNQfy(filename = 'UNQfy/data.json') {
+function getUNQfy(filename = '../../data.json') {
         let unqfy = new unqmod.UNQfy();
         if (fs.existsSync(filename)) {
             unqfy = unqmod.UNQfy.load(filename);
-            console.log("ACAAAAAAAAAAAAAA", unqmod.UNQfy);
-            console.log("AQUIIIIIIIIIIIIII", unqmod.UNQfy.getArtists());
         }
         return unqfy;
 }
   
-function saveUNQfy(unqfy, filename = 'data.json'){
+function saveUNQfy(unqfy, filename = '../../data.json'){
         unqfy.save(filename);
 }
 
