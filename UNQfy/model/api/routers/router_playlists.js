05c2a9232b8fcf062de19369e762b2781b11e785
underabloodysky <valenzuelahoracioe@gmail.com>
2020-10-30 05:58:33 -0300

    Fix de endpoints de play lists terminados con los tests en verde

--- UNQfy/model/api/routers/router_playlists.js
+++ UNQfy/model/api/routers/router_playlists.js
@@ -8,52 +8,28 @@ playList_router.route('/playlists')
     .post((req, res) =>{
         const unqfy = getUNQfy();
         const playlist_data = req.body;
-        if (playlist_data.name === undefined || playlist_data.tracks === undefined){
+        if (playlist_data.maxDuration === undefined || playlist_data.genres === undefined){
             res.status(400);
             res.json({status: 400,
                 errorCode: "BAD_REQUEST"});
         }
-        try{
-            if (playlist_data.maxDuration != undefined && playlist_data.genres != undefined) {
-                const aList = unqfy.createPlaylist(playlist_data.name, playlist_data.genres, playlist_data.maxDuration);
+        else{
+            try{
+                const aPlayList = unqfy.createPlaylist(playlist_data.name, playlist_data.genres, playlist_data.maxDuration);
                 saveUNQfy(unqfy);
                 res.status(201);
-                res.json({
-                    "id": aList.id,
-                    "name": aList.name,
-                    "duration": aList.duration,
-                    "tracks": aList.tracks,
-                });
-            } else{
-                const listByTracks = new playList(playlist_data.name, playlist_data.tracks);
-                const haveSomeTrack = unqfy.getTracks().some(track => track.name === playlist_data.tracks);
-                if(haveSomeTrack){
-                    let aList = new PlayList(playlist_data.name, playlist_data.tracks);
-                    saveUNQfy(unqfy);
-                    res.status(201);
-                    res.json({
-                        "id": listByTracks.id,
-                        "name" : listByTracks.name,
-                        "duration": listByTracks.duration,
-                        "tracks": listByTracks.tracks,
-                    })
-                } else {
-                    res.status(409);
-                    res.json({
-                        status: 409,
-                        errorCode: "WRONG PARAMETERS"
-                    });
-                }
+                res.json(
+                    aPlayList.toJSON()
+                );}
+            catch (e) {
+                throw e;
             }
         }
-        catch (e) {
-            throw res.send('Error: ', e);
-        }
-    })
-playList_router.route('/playlists/:idPlaylists')
+    });
+playList_router.route('/playlists/:id_playlist')
     .get((req, res) =>{
         const unqfy = getUNQfy();
-        const playlist_id = parseInt(req.params.id);
+        const playlist_id = parseInt(req.params.id_playlist);
         const playlist = unqfy.getPlaylistById(playlist_id);
         if (playlist === undefined){
             res.status(405);
@@ -63,12 +39,12 @@ playList_router.route('/playlists/:idPlaylists')
             res.json({
                 status: 200,
                 playlist : playlist.toJSON()
-            })
+            });
         }
     })
     .delete((req,res) =>{
         const unqfy = getUNQfy();
-        const playlist_id = parseInt(req.params.id);
+        const playlist_id = parseInt(req.params.id_playlist);
         try{
             unqfy.removePlayList(playlist_id);
             saveUNQfy(unqfy);
@@ -84,30 +60,23 @@ playList_router.route('/playlists/:idPlaylists')
                 throw e;
             }
         }
-    })
+    });
 
 playList_router.route('/playlists')
     .get((req,res)=>{
         const unqfy = getUNQfy();
         const playlist_data = req.query;
-        let playLists = unqfy.getPlayLists();
-
-        try {
-            if (playlist_data.name != undefined) {
-                playLists.some(playlist => playlist.name === playlist_data.name);
-            }
-            if (playlist_data.durationLT != undefined) {
-                playLists = playLists.filter(playList => playList.duration < playlist_data.durationLT);
-            }
-            if (playlist_data.durationGT != undefined) {
-                playLists = playLists.filter(playList => playList.duration > playlist_data.durationGT);
-            }
-            res.status(200);
-            res.json(playLists.map(playList => playList.toJSON()));
+        if(playlist_data.name === undefined && playlist_data.durationLT === undefined && playlist_data.durationGT === undefined){
+            res.status(400);
+            res.json({ status: 400,
+                errorCode: "BAD_REQUEST"});
+                return;
         }
-        catch (e) {
-            throw e;
-        }
-    })
+        console.log("aca");
+        const name = playlist_data.name !== undefined ? "" : playlist_data.name;
+        const playLists = unqfy.searchPlaylistsWithPartialName(name).filter(playList => ! (playList.duration < playlist_data.durationLT || playList.duration > playlist_data.durationGT));
+        res.status(200);
+        res.json(playLists.map(playList => playList.toJSON()));
+    });    
 
 module.exports = playList_router;
